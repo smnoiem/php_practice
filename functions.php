9634ebc87b20f8cb4122d2ccac3f967377bcf1d8
Noiem Uddin <smnoiem@gmail.com>
2023-02-25 13:25:10 +0600

    superglobals and custom public functions and importing

---
+++ functions.php
@@ -0,0 +1,14 @@
+<?php
+
+function dd($value) 
+{
+    echo "<pre>";
+    var_dump($value);
+    echo "</pre>";
+    die();
+}
+
+function isUri($uri)
+{
+    return $_SERVER['REQUEST_URI'] === $uri;
+}
--- views/partials/nav.php
+++ views/partials/nav.php
@@ -9,11 +9,11 @@
             <div class="hidden md:block">
                 <div class="ml-10 flex items-baseline space-x-4">
                 <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-                <a href="/" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium" aria-current="page">Home</a>
+                <a href="/" class="<?= isUri('/') ? "bg-gray-900 text-white" : "text-gray-300 hover:bg-gray-700 hover:text-white" ?> px-3 py-2 rounded-md text-sm font-medium" aria-current="page">Home</a>
 
-                <a href="/about.php" class="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium">About</a>
+                <a href="/about.php" class="<?= isUri('/about.php') ? "bg-gray-900 text-white" : "text-gray-300 hover:bg-gray-700 hover:text-white" ?> px-3 py-2 rounded-md text-sm font-medium">About</a>
 
-                <a href="/contact.php" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Contact</a>
+                <a href="/contact.php" class="<?= isUri('/contact.php') ? "bg-gray-900 text-white" : "text-gray-300 hover:bg-gray-700 hover:text-white" ?> px-3 py-2 rounded-md text-sm font-medium">Contact</a>
                 </div>
             </div>
             </div>

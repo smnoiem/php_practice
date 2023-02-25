2e8f2fa2400462b861e6308917cfd04dd9b350b9
Noiem Uddin <smnoiem@gmail.com>
2023-02-25 21:01:34 +0600

    moving pdo setup into a Database class

--- db.php
+++ db.php
@@ -1,22 +1,34 @@
 <?php
 
-$dsn = "mysql:host=localhost;port=3306;dbname=php_practice;charset=utf8mb4";
-
-try{
-    $pdo = new PDO($dsn, 'root', 'bd7toRy5%');
-}
-catch(Exception $exception) {
-    echo "db connection failed: {$exception}";
-    die();
-}
-
-$prepared = $pdo->prepare("select * from posts");
-
-$prepared->execute();
-
-$posts = $prepared->fetchAll(PDO::FETCH_ASSOC);
-
-foreach($posts as $post)
+class Database
 {
-    echo "<li>{$post['title']}</li>";
+    public $connection;
+
+    public function __construct()
+    {
+        $dsn = "mysql:host=localhost;port=3306;dbname=php_practice;charset=utf8mb4";
+
+        try {
+            $this->connection = new PDO($dsn, 'root', 'bd7toRy5%');
+        }
+        catch(Exception $exception) {
+            echo "db connection failed: {$exception}";
+            die();
+        }
+    }
+
+    public function query($query)
+    {
+        $prepared = $this->connection->prepare($query);
+
+        $prepared->execute();
+
+        return $prepared;
+    }
 }
+
+$db = new Database();
+
+$post = $db->query("select * from posts where id = 1")->fetch(PDO::FETCH_ASSOC);
+
+echo "<li>{$post['title']}</li>";

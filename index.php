307608b95dbd51d2fe2e13e0332e8be9392a90ac
Noiem Uddin <smnoiem@gmail.com>
2023-02-25 20:37:41 +0600

    basic pdo setup

--- index.php
+++ index.php
@@ -2,4 +2,25 @@
 
 require "functions.php";
 
-require "router.php";
+// require "router.php";
+
+$dsn = "mysql:host=localhost;port=3306;dbname=php_practice;charset=utf8mb4";
+
+try{
+    $pdo = new PDO($dsn, 'root', 'bd7toRy5%');
+}
+catch(Exception $exception) {
+    echo "db connection failed: {$exception}";
+    die();
+}
+
+$prepared = $pdo->prepare("select * from posts");
+
+$prepared->execute();
+
+$posts = $prepared->fetchAll(PDO::FETCH_ASSOC);
+
+foreach($posts as $post)
+{
+    echo "<li>{$post['title']}</li>";
+}

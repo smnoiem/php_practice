0969fc9cf7f6852954b75b8479b8e755b7e4ee8b
Noiem Uddin <smnoiem@gmail.com>
2023-02-27 20:25:13 +0600

    bug handled on the /db page

--- db.php
+++ db.php
@@ -6,6 +6,11 @@ $config = require("config.php");
 
 $db = new Database($config['database'], 'root', 'bd7toRy5%');
 
+if(!isset($_GET['id'])) {
+    echo "id isn't passed in the query parameters";
+    return;
+}
+
 $id = $_GET['id'];
 
 $query = "select * from posts where id = :id";

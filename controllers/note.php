792aee23d6a3347fb5794037e1c5c49cd8b0bd09
Noiem Uddin <smnoiem@gmail.com>
2023-02-27 21:56:50 +0600

    authorization intro and Response class with const

--- controllers/note.php
+++ controllers/note.php
@@ -4,7 +4,14 @@ $config = require("config.php");
 $db = new Database($config['database'], 'root', 'bd7toRy5%');
 
 $heading = "Note";
+$currentUserId = 6;
 
-$note = $db->query("select * from notes where id = :id", ['id' => $_GET['id']])->fetch();
+$note = $db->query("select * from notes where id = :id", [
+    'id' => $_GET['id'],
+])->fetch();
+
+if(!$note) abort(404);
+
+if($note['user_id'] !== $currentUserId) abort(Response::FORBIDDEN);
 
 require 'views/note.view.php';

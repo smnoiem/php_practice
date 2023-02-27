c0fa1edfc26bc1620e077f9c5dc8c926406e3887
Noiem Uddin <smnoiem@gmail.com>
2023-02-27 11:50:33 +0600

    db connect and query handling moved to separate Database class, moved in a separate file

--- db.php
+++ db.php
@@ -1,31 +1,6 @@
 <?php
 
-class Database
-{
-    public $connection;
-
-    public function __construct()
-    {
-        $dsn = "mysql:host=localhost;port=3306;dbname=php_practice;charset=utf8mb4";
-
-        try {
-            $this->connection = new PDO($dsn, 'root', 'bd7toRy5%');
-        }
-        catch(Exception $exception) {
-            echo "db connection failed: {$exception}";
-            die();
-        }
-    }
-
-    public function query($query)
-    {
-        $prepared = $this->connection->prepare($query);
-
-        $prepared->execute();
-
-        return $prepared;
-    }
-}
+require "Database.php";
 
 $db = new Database();
 

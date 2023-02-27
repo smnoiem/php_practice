d7875d5a0b8da56dbb202e6d357c925dd40f2922
Noiem Uddin <smnoiem@gmail.com>
2023-02-28 00:07:16 +0600

    basic form request validation

--- controllers/notes-create.php
+++ controllers/notes-create.php
@@ -6,10 +6,23 @@ $db = new Database($config['database'], 'root', 'bd7toRy5%');
 $heading = "Create Note";
 
 if($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $db->query("INSERT INTO notes(body, user_id) VALUES(:body, :user_id)", [
-        'body' => $_POST['body'],
-        'user_id' => 6,
-    ]);
+
+    $errors = [];
+
+    if(strlen($_POST['body']) === 0) {
+        $errors['body'] = 'A body is required';
+    }
+
+    if(strlen($_POST['body']) > 1000) {
+        $errors['body'] = 'The body cannot be more than 1000 characters';
+    }
+
+    if(empty($errors)) {
+        $db->query("INSERT INTO notes(body, user_id) VALUES(:body, :user_id)", [
+            'body' => $_POST['body'],
+            'user_id' => 6,
+        ]);
+    }
 }
 
 require 'views/notes-create.view.php';

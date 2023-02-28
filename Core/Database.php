6f191e2bbd9c766af32c76faefd5dbce673c89b2
Noiem Uddin <smnoiem@gmail.com>
2023-02-28 15:24:03 +0600

    intro to namespace

--- Core/Database.php
+++ Core/Database.php
@@ -1,5 +1,10 @@
 <?php
 
+namespace Core;
+
+use PDO;
+use Exception;
+
 class Database
 {
     public $connection;
--- Core/Response.php
+++ Core/Response.php
@@ -1,5 +1,7 @@
 <?php
 
+namespace Core;
+
 class Response
 {
     const FORBIDDEN = 403;

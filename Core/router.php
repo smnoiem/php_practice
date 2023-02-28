5d063b854806a9b51e2c4d8b8e204874aa61149c
Noiem Uddin <smnoiem@gmail.com>
2023-02-28 15:08:39 +0600

    fix: controller base path is handled in the router

--- Core/router.php
+++ Core/router.php
@@ -5,7 +5,7 @@ $routes = require(base_path("routes.php"));
 function routeToController($uri, $routes)
 {
     if(array_key_exists($uri, $routes)) {
-        require $routes[$uri];
+        require base_path($routes[$uri]);
     }
     else {
         abort();

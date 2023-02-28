69cc292db3c2257507dda4bfdfad66b6d3f56caa
Noiem Uddin <smnoiem@gmail.com>
2023-02-28 13:07:26 +0600

    resourceful naming

--- controllers/notes/index.php
+++ controllers/notes/index.php
@@ -7,4 +7,4 @@ $heading = "My Notes";
 
 $notes = $db->query("select * from notes where user_id = :id", ['id' => 6])->get();
 
-require 'views/notes.view.php';
+require 'views/notes/index.view.php';
--- routes.php
+++ routes.php
@@ -4,8 +4,8 @@ return [
     '/' => 'controllers/index.php',
     '/contact' => 'controllers/contact.php',
     '/about' => 'controllers/about.php',
-    '/notes' => 'controllers/notes.php',
-    '/notes/create' => 'controllers/notes-create.php',
-    '/note' => 'controllers/note.php',
+    '/notes' => 'controllers/notes/index.php',
+    '/notes/create' => 'controllers/notes/create.php',
+    '/note' => 'controllers/notes/show.php',
     '/db' => 'db.php',
 ];
